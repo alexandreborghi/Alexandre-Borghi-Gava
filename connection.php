5725fcada72e122539fbfef4dcc49519d23bee28
alexandreborghi <alexandreborghii20@gmail.com>
2023-03-27 21:14:54 -0300

    Conexao com banco de dados

--- connection.php
+++ connection.php
@@ -8,7 +8,20 @@
 </head>
 <body>
     <?php
-        echo "hello\n";
+        echo "hello world\n";
+        // Substitua pelos seus próprios valores de configuração
+        $host = 'localhost:3000';
+        $dbname = 'EMD 101';
+        $username = 'nome_de_usuario';
+        $password = '1234';
+
+// Cria a conexão com a base de dados
+        try {
+            $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        } catch (PDOException $e) {
+            die("Erro ao conectar com a base de dados: " . $e->getMessage());
+        }
+
     ?>
 
 
